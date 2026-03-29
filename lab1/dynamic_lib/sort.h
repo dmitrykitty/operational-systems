9ecb0015ee123c7333e564209fbb97f1c89c4bfc
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-03-29 20:01:15 +0200

    refactored files storage

--- lab1/dynamic_lib/sort.h
+++
@@ -1,6 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-
-void bubble_sort(float*, int);
-
-#endif
--- lab1/static_lib/sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-
-void bubble_sort(float*, int);
-void selection_sort(float*, int);
-
-#endif
